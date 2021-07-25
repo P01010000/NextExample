3db144572589e58c461eb67df3846fbb3a096a40
P01010000 <heiner.poepping@t-online.de>
2021-07-25 23:24:25 +0200

    Add cache-header for hello-endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  res.setHeader('Cache-Control', 'must-revalidate');
+  res.setHeader('Expires', new Date(Date.now() + 60 * 1000).toUTCString());
   res.status(200).json({ name: 'John Doe' })
 }
